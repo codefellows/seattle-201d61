0a5e31a2039c7962454af6dae5e51a18b8f7d1dc
Peyton-Cysewski <pcysewski12@gmail.com>
2020-03-06 14:47:13 -0800

    hmmmm

--- class-05/lab/starter-code/app.js
+++ class-05/lab/starter-code/app.js
@@ -81,6 +81,7 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+    
 
 }
 
