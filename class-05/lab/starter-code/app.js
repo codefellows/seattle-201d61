9eda80909b1e92cb445c993c90c92594efe962c5
Peyton-Cysewski <pcysewski12@gmail.com>
2020-03-06 15:46:50 -0800

    did problem 5, but accidentally did problem 6 as well

--- class-05/lab/starter-code/app.js
+++ class-05/lab/starter-code/app.js
@@ -81,6 +81,7 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+    // I didn't realize the array was limited to three items in the explanation, thus the comlexity.
     var numList = '' + sumArr[0];
     var arrAdded = 0;
 
@@ -99,7 +100,7 @@ function sumArray(sumArr) { //eslint-disable-line
 
 // Here is the test for sumArray(); uncomment it to run it
 
-testSumArray(testArray);
+// testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -115,7 +116,21 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
+    // I didn't realize the array was limited to three items in the explanation, thus the comlexity.
+    var numList = '' + multArr[0];
+    var arrMulted = 1;
 
+    for (var i = 1; i < multArr.length; i++) {
+        numList = numList +  ',' + multArr[i];
+    }
+
+    for (var i = 0; i < multArr.length; i++) {
+        arrMulted = multiply(arrMulted, multiply(1, multArr[i])[0])[0];
+    }
+
+    var resp = 'The numbers ' + numList + ' have a product of ' + arrMulted + '.'
+
+    return [arrMulted, resp]
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
@@ -143,7 +158,20 @@ Test this function by hand in the console to get it working, and when you think 
 var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
+    var numList = '' + dynamicArray[0];
+    var arrMulted = 1;
 
+    for (var i = 1; i < dynamicArray.length; i++) {
+        numList = numList +  ',' + dynamicArray[i];
+    }
+
+    for (var i = 0; i < dynamicArray.length; i++) {
+        arrMulted = multiply(arrMulted, multiply(1, dynamicArray[i])[0])[0];
+    }
+
+    var resp = 'The numbers ' + numList + ' have a product of ' + arrMulted + '.'
+
+    return [arrMulted, resp]
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
