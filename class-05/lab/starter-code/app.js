ba39995125e89253611f27406d2c252460b052f3
Paul Depew <pauldepew@comcast.net>
2020-03-06 14:13:10 -0800

    added array test

--- class-05/lab/starter-code/app.js
+++ class-05/lab/starter-code/app.js
@@ -59,18 +59,18 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
-  var added = (a + b + c);
-  var multiplied = (a * b * c);
-  var response1 = ( a + ' and ' + b + ' and ' + c + ' sum to ' + added + '.');
-  var response2 = ('The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multiplied + '.');
-  var array = [added, multiplied, response1, response2];
+// function sumAndMultiply(a, b, c) { //eslint-disable-line
+//   var added = (a + b + c);
+//   var multiplied = (a * b * c);
+//   var response1 = ( a + ' and ' + b + ' and ' + c + ' sum to ' + added + '.');
+//   var response2 = ('The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multiplied + '.');
+//   var array = [added, multiplied, response1, response2];
 
-  return array ;
-}
+//   return array ;
+// }
 
-// Here is the test for sumAndMultiply(); uncomment it to run it
-testSumAndMultiply(4,7,5);
+// // Here is the test for sumAndMultiply(); uncomment it to run it
+// testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -88,12 +88,15 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+    var added=(testArray[0] + testArray[1] + testArray[2]);
+    var response = (testArray[0] + "," + testArray[1] + "," + testArray[2] + " was passed in as an array of numbers, and " + added + " is their sum.");
+    var array = [added, response];
+    return array;
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
