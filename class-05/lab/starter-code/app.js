53e890c8d47c0600d21f8dd8bb31a1235ebdc7d7
Peyton-Cysewski <pcysewski12@gmail.com>
2020-03-06 14:19:46 -0800

    did problem 3

--- class-05/lab/starter-code/app.js
+++ class-05/lab/starter-code/app.js
@@ -11,7 +11,7 @@ Test this function by hand in the console to get it working, and when you think 
 function sum(a, b) { //eslint-disable-line
     var num = a + b;
     var string = 'The sum of ' + a + ' and ' + b + ' is ' + num + '.';
-    return [num, string]
+    return [num, string];
 }
 
 // Here is the test for sum(); uncomment it to run it
@@ -31,11 +31,11 @@ Test this function by hand in the console to get it working, and when you think 
 function multiply(a, b) { //eslint-disable-line
     var num = a * b;
     var string = 'The product of ' + a + ' and ' + b + ' is ' + num + '.';
-    return [num, string]
+    return [num, string];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+//testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -52,11 +52,18 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+    var added = a + b + c;
+    var prod = a * b * c;
+
+    var addString = a + ' and ' + b + ' and ' + 5 + ' sum to ' + added + '.';
+    var prodString ='The product of ' + a + ' and ' + b + ' and ' + 5 + ' is ' + prod+ '.';
+
+    return [added, prod, addString, prodString];
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+//testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
