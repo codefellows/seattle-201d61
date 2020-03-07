948c96ad138c8f970460b9c44320a4a8652d17a5
Pale-Crusader <meticulous.planning@gmail.com>
2020-03-06 17:07:37 -0800

    Problem 3 being commited after successful test

--- class-05/lab/starter-code/app.js
+++ class-05/lab/starter-code/app.js
@@ -57,11 +57,26 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+    console.log ('at beginning' + a +'. '+ b +'. '+ c)
+    var firstMultiply = multiply(a, b);
+    var tripleProductArray = multiply(firstMultiply[0], c);
+    var endProduct = tripleProductArray[0];
+    // endProduct = (endProduct + '')
+    var tripleMultiplyStatement = ('The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + endProduct + '.');
 
+    var firstSum = sum(a, b);
+    var tripleSumArray = sum(firstSum[0], c);
+    var endSum = tripleSumArray[0];
+    // endSum = (endSum + '');
+
+    var tripleSumStatement = (+ a + ' and ' + b + ' and ' + c + ' sum to ' + endSum + '.');
+    var tripleProblemArray = [endSum, endProduct, tripleSumStatement, tripleMultiplyStatement];
+    return tripleProblemArray;
 }
+sumAndMultiply(4, 7, 5);
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
