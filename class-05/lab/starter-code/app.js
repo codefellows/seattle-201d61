bb028670e44c1187c1bf29136efa4787a5806c86
Paul Depew <pauldepew@comcast.net>
2020-03-06 16:25:46 -0800

    update multiply

--- class-05/lab/starter-code/app.js
+++ class-05/lab/starter-code/app.js
@@ -34,7 +34,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
+    var result = (a * b);
+    var response = ('The product of ' + a + ' and ' + b + ' is ' + result + '.');
 
+    var array = [result, response];
+
+    return array;
+
+}
 }
 
 // Here is the test for multiply(); uncomment it to run it
