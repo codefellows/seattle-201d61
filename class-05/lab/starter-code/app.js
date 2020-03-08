aea649424320e1e62d2d0a53fdad8aebb0911173
‘penjoe’ <pennockjoe@gmail.com>
2020-03-08 13:17:06 -0700

    added code to solve problem #1

--- class-05/lab/starter-code/app.js
+++ class-05/lab/starter-code/app.js
@@ -9,11 +9,18 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
+  var value = (a + b);
+  var results = [];
 
+    if (value === 11) {
+      results.push(value);
+      results.push('The sum of ' + a + ' and ' + b  + ' is ' + value + '.')
+    }
+    return results;
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
