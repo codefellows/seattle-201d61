144b628c576623aa176c4382a15f12c2efe760b9
Haley Griffin <haleygriffin708@gmail.comgit config --global user.name h-griffin>
2020-03-07 22:52:39 -0800

    problem 4

--- class-05/lab/starter-code/app.js
+++ class-05/lab/starter-code/app.js
@@ -20,7 +20,7 @@ function sum(a, b) { //eslint-disable-line
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -42,7 +42,7 @@ function multiply(a, b) { //eslint-disable-line
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -75,8 +75,6 @@ function sumAndMultiply(a, b, c) { //eslint-disable-line
     trippleProductArray = [endAdd, endProduct, trippleAddArrayStatement, trippleMultiplyStatement];
     console.log(trippleProductArray);
     return trippleProductArray; 
-
-
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
@@ -96,14 +94,30 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 var testArray = [2, 3, 4]; //eslint-disable-line
+// var sumArr = testArray;
 
 function sumArray(sumArr) { //eslint-disable-line
+    // var sumArr = testArray;
+    console.log(sumArr);
+    var firstSum = sum(testArray[0], testArray[1]);
+    var thirdSumArray = sum(firstSum[0], testArray[2]);
+    var endThreeSum = thirdSumArray[0];
+    
+    var threeSumArrayStatement = (2 + ',' + 3 + ',' + 4 + ' was passed in as an array of numbers, and ' + endThreeSum + ' is their sum.');
 
+    console.log(firstSum);
+    console.log(thirdSumArray);
+    console.log(threeSumArrayStatement);
+    // console.log(sumArr);
+    var sumEnd = [endThreeSum, threeSumArrayStatement];
+    console.log(sumEnd);
+
+    return sumEnd;
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
